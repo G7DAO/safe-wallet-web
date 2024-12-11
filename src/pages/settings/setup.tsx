e6021143fa9737c8e3ef39bcdc6268948fec624e
msvstj <savastijonok@gmail.com>
2024-12-11 14:42:48 +0200

    fix: title

--- src/pages/settings/setup.tsx
+++ src/pages/settings/setup.tsx
@@ -19,7 +19,7 @@ const Setup: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Game7 Safengs – Setup</title>
+        <title>Game7 Safe – Setup</title>
       </Head>
 
       <SettingsHeader />
