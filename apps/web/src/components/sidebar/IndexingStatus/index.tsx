ed946a1abdc8cc8f87f180e6e94c064e60592da9
ajimeno04 <jimenobeltran11@gmail.com>
2025-03-27 12:36:00 +0100

    refactor: rename interval counter variable for clarity

--- apps/web/src/components/sidebar/IndexingStatus/index.tsx
+++ apps/web/src/components/sidebar/IndexingStatus/index.tsx
@@ -12,7 +12,7 @@ const POLL_INTERVAL = 1000 * 60 // 1 minute
 
 const useIndexingStatus = () => {
   const chainId = useChainId()
-  const [count] = useIntervalCounter(POLL_INTERVAL)
+  const [_count] = useIntervalCounter(POLL_INTERVAL)
 
   return useAsync(
     () => {
